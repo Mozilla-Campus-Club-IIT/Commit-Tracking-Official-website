77611458830f49c41cd522dae8a1c625bb168790
wdevon99 <wdevon99@gmail.com>
2018-05-20 22:04:07 +0530

    Deleted all

--- server/server.js
+++
@@ -1,8 +0,0 @@
-const http = require('http'); //importing hhtp package
-const app = require('./app'); //importing app  
-
-const port = process.env.PORT || 3000;
-
-const server = http.createServer(app);
-
-server.listen(port);
