6ccd1973e49f2e92e31059a0d50ce1a4936a96c7
RavinduHasithanjana <ravinduceylofana@outlook.com>
2018-07-17 21:47:04 +0530

    contact us page UI done

--- Angular-FrontEnd/src/app/app.module.ts
+++ Angular-FrontEnd/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule ,Routes} from '@angular/router'
+import { RouterModule ,Routes} from '@angular/router';
+
+
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
